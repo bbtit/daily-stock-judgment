775b5285363220872338b7cf96e3f972e920748e
bbtit <the8butterfly8dream@gmail.com>
2026-08-02 00:05:03 +0900

    chore: update Lighthouse CI configuration to reduce number of runs from 3 to 1
    Modified the lighthouserc.js file to streamline the Lighthouse CI process by decreasing the number of runs, optimizing performance for local testing.

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,7 +5,7 @@ module.exports = {
       startServerCommand:
         "DSJ_MARKET=demo DSJ_JUDGMENT_MODEL=demo DSJ_AS_OF=2026-07-31 DSJ_DB_PATH=data/lhci.db uv run daily-stock-judgment",
       startServerReadyPattern: "Uvicorn running",
-      numberOfRuns: 3,
+      numberOfRuns: 1,
     },
     upload: {
       target: "filesystem",
